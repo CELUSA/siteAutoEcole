f2cb849c120e005a9e142431ebbed4c0d93df7c5
NtHxLukas <lucas.gardon@gmail.com>
2014-04-14 10:05:18 +0200

    Create 1e.php
    Ajout de la page permis a un euro.

---
+++ 1e.php
@@ -0,0 +1,70 @@
+<!DOCTYPE HTML>
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Auto-Ecole Driver</title>
+<META NAME="description" CONTENT="Auto-Ecole Paris"> 
+<META NAME="keywords" CONTENT="Auto, école, conduite,  Paris, Paris 1e ">
+        <meta name="robots" content="index,follow" />
+        <meta property="og:title" content="Driver, Paris 1er"/>
+        <meta property="og:type" content="autoecole" />
+        <meta property="og:latitude" content="2.358404"/>
+        <meta property="og:longitude" content="48.856022"/>
+        <meta property="og:street-address" content="24 rue de Rivoli"/>
+        <meta property="og:locality" content="Paris 1ème"/>
+        <meta property="og:region" content="Île-de-France"/>
+        <meta property="og:postal-code" content="75001"/>
+        <meta property="og:country-name" content="France"/>
+		
+		<!-- Include Main jQuery library for slider -->
+        <script type="text/Javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+        <!-- Include Main Easy Slides library & default css file-->
+        <script type="text/Javascript" src="jQuery.easySlides/js/jquery.easyslides.min.v1.1.js"></script>
+        <link rel="stylesheet" type="text/css" href="jQuery.easySlides/css/easySlides.default.min.css" />
+        <!-- Include own Javascript/jQuery & CSS file for Slider -->
+        <script type="text/Javascript" src="Examples/Example 1/example_1.js"></script>
+        <link rel="stylesheet" type="text/css" href="Examples/Example 1/styles.css" />
+		<!-- Include CSS stylesheet for this page -->
+        <link rel="stylesheet" type="text/css" href="index.css" />
+        
+</head>
+
+<header>
+
+<!--header-->
+<?php require 'header.html'; ?>
+
+</header>
+
+<body class="page">
+<div class="container">
+
+ <div class="content">
+
+<div style=" margin:15px;text-align:justify"><p> <div>
+
+<h3><center>Notre auto école vous propose cette opportunité ouverte à tous</center></h3>
+<p> <center><img alt="" src="images/1e.png"></center><br><p>Avec 700 000 candidats reçus chaque année, le permis de conduire est le premier examen en France.</p>
+<p>Parce qu’un grand nombre de jeunes de 16 à 25 ans n’ont pas toujours les fonds nécessaires pour le financement de leur formation à la conduite, l’Etat a mis en place le permis à un euro par jour.<br>
+L’objectif est simple : favoriser l’accès des jeunes au permis de conduire, à travers un système de prêt à taux zéro avec un remboursement d’un euro par jour.</p>
+<p>Le coût total de la formation au permis de conduire ne change pas, mais l’établissement financier avance l’argent et l’Etat paie les intérêts.</p>
+<p>Tous les jeunes qui ont entre 16 et 25 ans révolus peuvent en bénéficier, sous réserve que l’établissement financier accepte leur dossier. Après avoir déterminé le nombre d’heures prévisionnel nécessaire, l’auto école établit un devis et&nbsp;un contrat pour la formation au permis de conduire. Ce contrat servira à formuler la demande de prêt auprès d’un établissement financier. Le remboursement (30 Euros par mois) commence dès que la somme est débloquée.</p>
+</div> </p></div>
+
+
+
+</div>
+
+  <!-- end .content -->
+
+  <!-- end .container --></div>
+
+</body>
+
+<footer>
+
+<!--footer-->
+<?php require 'footer.html'; ?>
+
+</footer>
+</html>
+
