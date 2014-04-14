06cb8afef642dd520f8db85b6069fd88f77b75db
NtHxLukas <lucas.gardon@gmail.com>
2014-04-14 10:06:39 +0200

    Create AutoEcole.php
    Ajout de la page AutoEcole.php

---
+++ AutoEcole.php
@@ -0,0 +1,65 @@
+<!DOCTYPE HTML>
+<html><head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Auto-Ecole Driver</title>
+<META NAME="description" CONTENT="Auto-Ecole Paris"> 
+<META NAME="keywords" CONTENT="Auto, école, conduite,  Paris, Paris 1e ">
+        <meta name="robots" content="index,follow" />
+        <meta property="og:title" content="Driver, Paris 1er"/>
+        <meta property="og:type" content="autoecole" />
+        <meta property="og:latitude" content="2.358404"/>
+        <meta property="og:longitude" content="48.856022"/>
+        <meta property="og:street-address" content="24 rue de Rivoli"/>
+        <meta property="og:locality" content="Paris 1ème"/>
+        <meta property="og:region" content="Île-de-France"/>
+        <meta property="og:postal-code" content="75001"/>
+        <meta property="og:country-name" content="France"/>
+
+
+        <!-- Include Main jQuery library for slider -->
+        <script type="text/Javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4/jquery.min.js"></script>
+        <!-- Include Main Easy Slides library & default css file-->
+        <script type="text/Javascript" src="jQuery.easySlides/js/jquery.easyslides.min.v1.1.js"></script>
+        <link rel="stylesheet" type="text/css" href="jQuery.easySlides/css/easySlides.default.min.css" />
+        <!-- Include own Javascript/jQuery & CSS file for Slider -->
+        <script type="text/Javascript" src="Examples/Example 1/example_1.js"></script>
+        <link rel="stylesheet" type="text/css" href="Examples/Example 1/styles.css" />
+		<!-- Include CSS stylesheet for this page -->
+        <link rel="stylesheet" type="text/css" href="index.css" />
+        
+</head>
+
+<header>
+
+<!--header-->
+<?php require 'header.html'; ?>
+
+</header>
+
+<body class="page">
+<div class="container">
+
+ <div class="content">
+<h1>Présentation de l'auto école</h1>
+
+<div style="padding:30px; margin:30px;text-align:justify"><p> L’auto-école « Driver » est une société à responsabilité limitée (S.A.R.L) en nom collectif fondée en décembre 1993 par Mme BROCHU, fondatrice et directrice de la société, avec un capital social de 70 000 euros. <br> <br>
+
+« Driver » est une auto-école possédant une solide réputation depuis plus de 20 ans. Fort de sa réputation, de nombreux jeunes s’inscrivent pour passer leur permis de conduire. Son principal atout, outre la compétence pédagogique de ses 7 moniteurs, est son offre très diversifiée de permis et tarifs. <br><br>
+
+L’auto-école possède au total 8 voitures (de type Clio 4 et 207) et 5 motos (de type ER6) avec un contrat de renouvellement applicable tous les 2 ans. Outre le permis B et A, l’auto-école propose  l’examen du B.S.R ainsi que la conduite accompagnée. <br><br>
+
+Driver est donc composée de Mme BROCHU, directrice de l’auto-école, ainsi que de 7 moniteurs qui sont responsables des élèves , de l’entraînement au code au passage de l’examen du permis. </p></div>
+
+</div>
+
+
+  <!-- end .container --></div>
+</body>
+<footer>
+
+<!--footer-->
+<?php require 'footer.html'; ?>
+
+</footer>
+</html>
+
